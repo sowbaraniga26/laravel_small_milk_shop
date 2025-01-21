e81276f0d5297eb8d99cb16257f0d0e1d2736a4a
sowbaraniga26 <sowbaraniga26@gmail.com>
2025-01-21 19:10:17 +0530

    Update Role Resource generate (Role resource.php manually added)
    php artisan make:filament-resource Role

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -17,18 +17,31 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Spatie\Permission\Models\Role;
 
 
-
 class RoleResource extends Resource
 {
     protected static ?string $model = Role::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-finger-print';
+
+    protected static ?int $navigationSort = 2;
+
+    protected static ?string $navigationGroup = 'Settings';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+
+                Forms\Components\Section::make('User Roles')
+                    ->description('Role Must be Minimum 2 Maximum 255 Character')
+                    ->schema([            
+                        Forms\Components\TextInput::make('name')
+                            ->minLength(2)
+                            ->maxLength(255)
+                            ->required()
+                            ->unique()
+                    ])
+
             ]);
     }
 
@@ -36,7 +49,7 @@ class RoleResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
             ])
             ->filters([
                 //
