c099135de0e061713cb31346adf2dabe6262d173
sowbaraniga26 <sowbaraniga26@gmail.com>
2025-01-22 19:41:32 +0530

    Laravel Sanctum User Model Config

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable,HasRoles;
+    use HasFactory, Notifiable,HasRoles,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
