0d6df24ab9e0ed1ac0b4c3b632f237a4de9b3a4c
sowbaraniga26 <sowbaraniga26@gmail.com>
2025-01-22 19:51:41 +0530

    ProductController API v2  listing of the products with their variants
    php artisan make:controller api/v2/ProductController

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
+
+use App\Http\Controllers\api\v2\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
