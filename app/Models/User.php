cb6a52a57d4eaa6426844c1854f7f44aa9fe0984
sowbaraniga26 <sowbaraniga26@gmail.com>
2025-02-18 19:14:42 +0530

    Get the full URL path for the icon added

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,9 +62,26 @@ class User extends Authenticatable
     }
 
     // Update Profile Pic Path
-    
+
     public function setImagePathAttribute($value)
     {
         $this->attributes['image_path'] = $value;
+    }
+
+    /**
+     * Get the full URL path for the icon.
+     *
+     * @param string $record
+     * @return string
+     */
+    public static function getFullPath($record)
+    {
+        // Assuming your images are stored in the public directory
+        
+        // $basePath = config('app.url') . '/storage/';
+        
+        $basePath = env('APP_URL') . '/storage/';
+
+        return $basePath . $record;
     }
 }
